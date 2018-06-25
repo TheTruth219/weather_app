9941afe9f79a0c334e543434296e0ecee7b9f791
TheTruth219 <36748331+TheTruth219@users.noreply.github.com>
2018-06-25 16:02:50 -0700

    initial commit

---
+++ profile.js
@@ -0,0 +1,43 @@
+const https =require('https');
+const http = require("http");
+
+function printError (error) {
+  console.error(error.message);
+}
+
+function printMessage(username, badgeCount, points) {
+  const message = `${username} has ${badgeCount} total badge(s) and ${points} points in JavaScript`;
+    console.log(message);
+}
+
+function get (username)  {
+  try {
+      const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
+        if (response.statusCode === 200) {
+            let body = "";
+            response.on('data', data =>{
+              body += data.toString();
+            });
+            response.on('end', () => {
+              try {
+                const profile =JSON.parse(body);
+                printMessage(username, profile.badges.length, profile.points.JavaScript);
+              }catch (error) {
+                printError(error);
+                }
+              });
+        }else {
+            const message = `The profile for ${username} was ${http.STATUS_CODES[response.statusCode].toLowerCase()}. Try someone you ACTUALLY know next time :)`;
+            const statusCodeError = new Error(message);
+            printError(statusCodeError);
+        }
+
+      });
+
+      request.on('error', printError);
+
+  } catch (error) {
+      printError(error);
+  }
+}
+module.exports.get = get;
